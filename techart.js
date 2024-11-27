cdf76056839b417ce9fb13a4c9afa2a48c7c52e0
depmkeu <candradevi2016@gmail.com>
2024-11-27 23:13:02 +0700

    New Update

--- techart.js
+++ techart.js
@@ -1,6 +1,30 @@
+// Fungsi untuk menampilkan dan menyembunyikan menu
 function toggleMenu() {
   const menu = document.querySelector(".menu-links");
   const icon = document.querySelector(".hamburger-icon");
   menu.classList.toggle("open");
   icon.classList.toggle("open");
+}
+
+// Fungsi untuk menampilkan tombol scroll to top
+const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+// Menampilkan atau menyembunyikan tombol scroll to top saat menggulir
+window.onscroll = function () {
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 100
+  ) {
+    scrollToTopBtn.style.display = "block";
+  } else {
+    scrollToTopBtn.style.display = "none";
+  }
+};
+
+// Fungsi untuk menggulir ke atas
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
 }
